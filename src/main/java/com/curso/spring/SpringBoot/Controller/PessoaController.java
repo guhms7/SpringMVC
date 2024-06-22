30a3a467e8cb96f1dae634823ec32133aa21d43f
Gustavo Moraes <guhms@outlook.com>
2024-06-22 14:42:40 -0300

    Finalizando o Crud

--- src/main/java/com/curso/spring/SpringBoot/Controller/PessoaController.java
+++ src/main/java/com/curso/spring/SpringBoot/Controller/PessoaController.java
@@ -1,11 +1,14 @@
 package com.curso.spring.SpringBoot.Controller;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.curso.spring.SpringBoot.Model.Pessoa;
 import com.curso.spring.SpringBoot.Repository.PessoaRepository;
@@ -16,23 +19,40 @@ public class PessoaController {
     @Autowired
     private PessoaRepository pessoaRepository;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/cadastroPessoa")
-    public String inicio(){
-        return "cadastro/cadastroPessoa";
+    @GetMapping("/cadastroPessoa")
+    public ModelAndView inicio(){
+        ModelAndView andView = new ModelAndView("cadastro/cadastroPessoa");
+        Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+        andView.addObject("pessoas", pessoasIt);
+        andView.addObject("pessoaObj", new Pessoa());
+        return andView;
     }
 
+    @GetMapping("/editarPessoa/{idPessoa}")
+    public ModelAndView editar(@PathVariable("idPessoa") Long idpessoa){
+        ModelAndView pessoaAndView = new ModelAndView("cadastro/cadastroPessoa");
+        Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+        pessoaAndView.addObject("pessoaObj", pessoa.orElse(new Pessoa()));
+        return pessoaAndView ;
+    }
     
-    @RequestMapping(method =RequestMethod.POST, value = "/salvarpessoa")
+    @GetMapping("/excluirPessoa/{idPessoa}")
+    public String excluir(@PathVariable("idPessoa") Long idPessoa) {
+        pessoaRepository.deleteById(idPessoa);
+        return "redirect:/cadastroPessoa";
+    }
+
+    @PostMapping("/salvarpessoa")
     public String salvar(Pessoa pessoa){
-        pessoaRepository.save(pessoa);
-        return "cadastro/cadastroPessoa";
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/cadastroPessoas")
-    public List<Pessoa> buscarTodos(){
-        return pessoaRepository.findAll();
+        pessoaRepository.saveAndFlush(pessoa);
+        return "redirect:/cadastroPessoa";
     }
     
-
-    
+    @PostMapping("/buscarNomePessoa")
+    public ModelAndView buscarNome(@RequestParam("buscarNomePessoa") String buscarNomePessoa){
+        ModelAndView andView = new ModelAndView("cadastro/cadastroPessoa");
+        andView.addObject("pessoas", pessoaRepository.findByNome(buscarNomePessoa));
+        andView.addObject("pessoaObj", new Pessoa());
+        return andView;
+    }
 }
